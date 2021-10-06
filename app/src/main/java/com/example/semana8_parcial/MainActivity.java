c0713b5837dec0655eee0b230b43e0735792a806
erika2201 <erikapapamija@gmail.com>
2021-10-06 11:58:53 -0500

    trying to connect to the server

--- app/src/main/java/com/example/semana8_parcial/MainActivity.java
+++ app/src/main/java/com/example/semana8_parcial/MainActivity.java
@@ -7,11 +7,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     //private TextView tittleText, nameTextView, cantTextView, posTextView,typeTextView;
     private EditText nameEditText, cantEditText, coordxEditText, coordyEditText;
     private Button redButton, greenButton, blueButton, deleteButton, createButton;
+
+    private Socket socket;
+    private BufferedWriter writer;
+    private BufferedReader reader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +39,66 @@ public class MainActivity extends AppCompatActivity {
         deleteButton = findViewById(R.id.deleteButton);
         createButton = findViewById(R.id.createButton);
 
+        initClient();
+
+    }
+
+    public void initClient() {
+        new Thread(
+                ()->{
+                    try {
+                        //Paso 2: Enviar solicitud de conexion
+                        socket = new Socket("192.168.0.32",2021);
+
+                        //Paso 3: Cliente y server conectados
+                        System.out.println("Se ha conectado al servidor!!!");
+
+                        OutputStream os = socket.getOutputStream();
+                        OutputStreamWriter osw = new OutputStreamWriter(os);
+                        writer = new BufferedWriter(osw);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        ).start();
+
+        colorButton();
+        deleteButton();
+        createButton();
+    }
+
+    public void colorButton (){
+       /* redButton.setOnClickListener(
+                v -> {
+
+                });
+
+
+        greenButton.setOnClickListener(
+                v -> {
+
+                });
+
+
+        blueButton.setOnClickListener(
+                v -> {
+
+                });*/
+    }
+
+    public void deleteButton(){
+        /*deleteButton.setOnClickListener(
+                v -> {
+
+                });*/
+    }
+
+    public void createButton(){
+       /* createButton.setOnClickListener(
+                v -> {
+
+                });*/
     }
 }
