de1c152d803a67f0dc639245b5fb526d8c848e10
erika2201 <erikapapamija@gmail.com>
2021-10-07 16:05:16 -0500

    Ya se se envían los mensajes desde android

--- app/src/main/java/com/example/semana8_parcial/MainActivity.java
+++ app/src/main/java/com/example/semana8_parcial/MainActivity.java
@@ -3,26 +3,35 @@ package com.example.semana8_parcial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.semana8_parcial.Model.Orden;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private TextView tittleText, nameTextView, cantTextView, posTextView,typeTextView;
     private EditText nameEditText, cantEditText, coordxEditText, coordyEditText;
     private Button redButton, greenButton, blueButton, deleteButton, createButton;
 
+    //Variables globales
     private Socket socket;
     private BufferedWriter writer;
     private BufferedReader reader;
+    private String name,color;
+    private int x,y,number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,30 @@ public class MainActivity extends AppCompatActivity {
 
         initClient();
 
+        redButton.setOnClickListener(
+                v -> {
+                    color = "ROJO";
+                   // Toast.makeText(this, color, Toast.LENGTH_SHORT).show();
+                });
+
+
+        greenButton.setOnClickListener(
+                v -> {
+                     color = "VERDE";
+                });
+
+
+        blueButton.setOnClickListener(
+                v -> {
+                    color = "AZUL";
+                });
+
+
+        createButton.setOnClickListener(
+                v -> {
+                    createButton();
+                });
+
     }
 
     public void initClient() {
@@ -48,57 +81,75 @@ public class MainActivity extends AppCompatActivity {
                 ()->{
                     try {
                         //Paso 2: Enviar solicitud de conexion
-                        socket = new Socket("192.168.0.32",2021);
-
+                        socket = new Socket("192.168.1.16",2021);
                         //Paso 3: Cliente y server conectados
                         System.out.println("Se ha conectado al servidor!!!");
+
+                        InputStream is = socket.getInputStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        reader = new BufferedReader(isr);
 
                         OutputStream os = socket.getOutputStream();
                         OutputStreamWriter osw = new OutputStreamWriter(os);
                         writer = new BufferedWriter(osw);
 
+                        while(true) {
+                            System.out.println("Esperando mensaje....");
+                            String line = reader.readLine();
+                            System.out.println("Recibido: " + line);
+
+
+                        }
+
+                    } catch (UnknownHostException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
                     } catch (IOException e) {
+                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                }
-
-        ).start();
-
-        colorButton();
-        deleteButton();
-        createButton();
+                }).start();
     }
 
     public void colorButton (){
-       /* redButton.setOnClickListener(
-                v -> {
-
-                });
-
-
-        greenButton.setOnClickListener(
-                v -> {
-
-                });
-
-
-        blueButton.setOnClickListener(
-                v -> {
-
-                });*/
+        name = nameEditText.getText().toString();
+        number = Integer.parseInt(cantEditText.getText().toString());
+        x = Integer.parseInt(coordxEditText.getText().toString());
+        y = Integer.parseInt(coordyEditText.getText().toString());
     }
 
     public void deleteButton(){
-        /*deleteButton.setOnClickListener(
+        deleteButton.setOnClickListener(
                 v -> {
-
-                });*/
+                   nameEditText.setText(" ");
+                   cantEditText.setText(" ");
+                   coordxEditText.setText(" ");
+                   coordyEditText.setText(" ");
+                });
     }
 
     public void createButton(){
-       /* createButton.setOnClickListener(
-                v -> {
+                    colorButton();
+                    Gson gson = new Gson();
+                    String json;
+                    Orden obj = new Orden(name, number, x, y, color);
+                    json = gson.toJson(obj);
+                    Toast.makeText(this, json, Toast.LENGTH_SHORT).show();
+                    sendMessage(json);
+                };
 
-                });*/
+    public void sendMessage(String msg) {
+        new Thread(
+                ()->{
+                    try {
+                        writer.write(msg+"\n");
+                        writer.flush();
+
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+        ).start();
     }
 }
---
+++ app/src/main/java/com/example/semana8_parcial/Model/Orden.java
@@ -0,0 +1,61 @@
+package com.example.semana8_parcial.Model;
+
+public class Orden {
+
+    String name;
+    int number;
+    int x, y;
+    String color;
+
+    public Orden(String name, int number, int x, int y, String color) {
+        this.name = name;
+        this.number = number;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public Orden() {
+        //Necesario para objeto Gson
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
